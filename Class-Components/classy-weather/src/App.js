32b36f1a2fe06f2e8a9f6deb144952799b1a86e9
Mehdi <hizenberg917@gmail.com>
2026-06-13 11:20:27 +0330

    Finished sec class base components

--- Class-Components/classy-weather/src/App.js
+++ Class-Components/classy-weather/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { getWeatherIcon, convertToFlag, formatDay } from "./starter";
+// This keyword behavior
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "Tehran",
-      isLoading: false,
-      displayLocation: "",
-      weather: {},
-    };
-    this.fetchWeather = this.fetchWeather.bind(this);
-  }
-  async fetchWeather() {
+  state = {
+    location: "Tehran",
+    isLoading: false,
+    displayLocation: "",
+    weather: {},
+  };
+
+  // async fetchWeather() {
+  fetchWeather = async () => {
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -41,22 +40,89 @@ class App extends React.Component {
     } finally {
       this.setState({ isLoading: false });
     }
-  }
+  };
+
+  setLocation = (e) => this.setState({ location: e.target.value });
+
   render() {
     return (
       <div className="app">
         <h1>Classy weather</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Search for location"
-            value={this.state.location}
-            onChange={(e) => this.setState({ location: e.target.value })}
-          />
-        </div>
+
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
+
         <button onClick={this.fetchWeather}>Get weather</button>
         {this.state.isLoading && <p className="loader">Loading...</p>}
+        {this.state.weather?.weathercode && (
+          <Weather
+            weather={this.state.weather}
+            location={this.state.displayLocation}
+          />
+        )}
       </div>
+    );
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Search for location"
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
+      </div>
+    );
+  }
+}
+
+class Weather extends React.Component {
+  // We use constructor method when we want to implement event-handler
+  render() {
+    const {
+      temperature_2m_max: max,
+      temperature_2m_min: min,
+      time: dates,
+      weathercode: codes,
+    } = this.props.weather;
+    return (
+      <div>
+        <h2>Weather {this.props.location}</h2>
+        <ul className="weather">
+          {dates.map((date, i) => (
+            <Day
+              date={date}
+              max={max.at(i)}
+              min={min.at(i)}
+              codes={codes.at(i)}
+              isToday={i === 0}
+              key={date}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Day extends React.Component {
+  render() {
+    const { date, max, min, codes, isToday } = this.props;
+    return (
+      <li className="day">
+        <span>{getWeatherIcon(codes)}</span>
+        <p>{isToday ? "Today" : formatDay(date)}</p>
+        <p>
+          {Math.floor(min)} &deg; &mdash;{" "}
+          <strong>{Math.ceil(max)} &deg;</strong>
+        </p>
+      </li>
     );
   }
 }
